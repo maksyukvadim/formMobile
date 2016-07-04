ffdedc781948a475563682d061f3d1b594bfb578
Vadym Maksyuk <maksyukvadim@gmail.com>
2016-07-04 14:06:11 +0300

    add new components

--- scripts.js
+++ scripts.js
@@ -1,4 +1,40 @@
+$(document).ready(function () {
+    $("#circleForBtn").hide();
+    $("#btnSingIn").click(function () {
+        $("#btnSingIn").animate({
+            width: "14",
+            fontSize: 0,
+            backgroundColor: "#ffff4b"
+        }, 300);
+        setTimeout(function () {
+            $("#circleForBtn").show();
+        }, 400);
+    });
 
-setInterval ( function() {
-    $('#btnSingIn').toggleClass('scale');
-},000);
+    setTimeout(function () {
+        $("#circleForBtn").hide();
+        $("#btnSingIn").animate({
+            fontSize: 0,
+            backgroundColor: "#ffff4b"
+        }, 10).addClass('ripple');
+    }, 5000);
+
+    $(".button-collapse").sideNav();
+    
+    var tempScrollTop, currentScrollTop = 0;
+    jQuery(window).scroll(function () {
+        currentScrollTop = jQuery(window).scrollTop();
+        if (tempScrollTop < currentScrollTop) {
+            $('#btnAdd').addClass('hideBtnAdd').removeClass("showBtnAdd");
+        }
+        else if (tempScrollTop > currentScrollTop) {
+            $('#btnAdd').addClass('showBtnAdd').removeClass("hideBtnAdd");
+        }
+        tempScrollTop = currentScrollTop;
+    })
+
+
+});
+
+
+
